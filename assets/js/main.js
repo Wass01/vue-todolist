c5c5f5cff9cd59bf74d3a42fd4132838b2177fb0
wass01 <maatallawassim01@gmail.com>
2021-03-25 16:53:19 +0100

    funzionamento check barra

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,19 +6,39 @@ var app = new Vue(
       todos: [
         {
           title: 'Inserire i miei todos',
+          status: 'todo',
         }
       ]
     },
+
+    // function per ordinare prima i todo da fare
+    // e poi quelli eliminati
+    computed:{
+      todoCpu: function() {
+        let todosOk = this.todos.filter((item) => item.status == 'todo');
+        let todosDel = this.todos.filter((item) => item.status == 'delete');
+
+        return[...todosOk, ...todosDel];
+      }
+    },
+
     methods: {
       // richiamao evento invio
       add: function() {
         if(this.utentText != ''){
           const obj = {
             title: this.utentText,
+            status: 'todo',
           }
           this.todos.push(obj);
           this.utentText = '';
         }
+      },
+
+      // click icon check
+      check: function(todo) {
+        let index = this.todos.indexOf(todo);
+        this.todos[index].status = 'delete';
       }
   }
   }
