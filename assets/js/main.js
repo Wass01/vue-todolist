f418d12041213b76d67f4bec2c94e4ff72940382
wass01 <maatallawassim01@gmail.com>
2021-03-25 15:45:56 +0100

    html css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,13 @@ var app = new Vue(
   {
     el: '#root',
     data: {
-
+      todos: [
+        {
+          title: 'Inserire i miei todos',
+        }
+      ]
     },
     methods: {
+  }
   }
 );
